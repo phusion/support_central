c0f53a0e54d8f9e9ff0c3d35195ffcfd3edf0b46
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-10-24 22:59:57 +0200

    Add layout

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def new
-    render status: 404, text: 'Action disabled'
-  end
-
   def create
     render status: 404, text: 'Action disabled'
   end
