cba285e4ad22c7aa5be58270079a5ed28dd0f9e7
Hongli Lai (Phusion) <hongli@phusion.nl>
2017-08-23 10:17:52 +0200

    Production deployment: upgrade to Ruby 2.3.4

--- config/deploy/phusion.rb
+++ config/deploy/phusion.rb
@@ -1,6 +1,6 @@
 server "support_central@internal.phusion.nl", :roles => [:app, :web, :db]
 
-set :rvm_ruby_version, '2.2.3'
+set :rvm_ruby_version, '2.3.4'
 set :repo_url, 'git://github.com/phusion/support_central.git'
 set :ssh_options, forward_agent: true
 set :passenger_environment_variables, { :path => '/opt/production/passenger-enterprise/bin:$PATH' }
